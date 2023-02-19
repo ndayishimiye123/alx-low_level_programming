ec7c8e7f56639add444fd594798cf6bbcdbe0e73
ndayishimiye <ndayishimiye123@gmail.com>
2023-02-19 12:16:08 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include <unistd.h>
-
+/**
+ * main - - Entry the correct point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, str, sizeof(str) - 1);
-  return (1);
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str, sizeof(str) - 1);
+return (1);
 }
