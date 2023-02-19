afd6ac4cb71fbdcdcd1a0ba6485210b9d76acbf2
ndayishimiye <ndayishimiye123@gmail.com>
2023-02-19 12:09:34 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - - Entry the correct point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    fputs("and that piece of art is useful\" - Dore korpar, 2015-10-19\n", stdout);
-    return 0;
+fputs("and that piece of art is useful\" - Dore korpar, 2015-10-19\n", stdout);
+return (0);
 }
