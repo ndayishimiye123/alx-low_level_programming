f6ac237c30ec765bc0a9a4bf7d05561320802fb6
ndayishimiye <ndayishimiye123@gmail.com>
2023-03-14 12:33:12 -0700

    Update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -15,50 +15,69 @@ for (c = 0; s[c] != '\0'; c++)
 if (s[c] == ' ')
 flag = 0;
 else if (flag == 0)
+{
 flag = 1;
-w++;
+w++
 }
 }
 return (w);
-}
-/**
- * **strtow - splits a string into words
+/**strtow - splits a string into words
  * @str: string to split
  *
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
+
+char **strtow(char *str)
+char **strtow(char *str, int max_words) {
 char **matrix, *tmp;
-int i, k = 0, len = 0, words, c = 0, start, end;
+int i, k = 0, len = 0, words = 0, c = 0, start = 0, end;
 while (*(str + len))
 len++;
-words = count_word(str);
-if (words == 0)
-return (NULL);
-matrix = (char **) malloc(sizeof(char *) * (words + 1));
-if (matrix == NULL)
-return (NULL);
-for (i = 0; i <= len; i++)
+for (i = 0; i < len; i++)
 {
-if (str[i] == ' ' || str[i] == '\0')
+if (str[i] == ' ' || str[i] == '\0') 
 {
-if (c)
+if (c) 
 {
-end = i;
-tmp = (char *) malloc(sizeof(char) * (c + 1));
-if (tmp == NULL)
-return (NULL);
-while (start < end)
-*tmp++ = str[start++];
-*tmp = '\0';
-matrix[k] = tmp - c;
-k++;
+words++;
 c = 0;
 }
 }
-else if (c++ == 0)
-start = i;
+else
+{
+c++;
+}
+}
+if (words == 0 || words > max_words)
+return NULL;
+matrix = (char **) malloc(sizeof(char *) * (words + 1));
+if (matrix == NULL)
+return NULL;
+for (i = 0; i < len; i++) {
+if (str[i] == ' ' || str[i] == '\0') {
+if (c) {
+end = i;
+tmp = (char *) malloc(sizeof(char) * (c + 1));
+if (tmp == NULL)
+return NULL;
+int j;
+for (j = 0; j < c; j++) {
+tmp[j] = str[start+j];
+}
+tmp[c] = '\0';
+matrix[k] = tmp;
+k++;
+c = 0;
+}
+start = i+1;
+}
+else
+{
+c++;
+}
 }
 matrix[k] = NULL;
-return (matrix);
-}
+return matrix;
+
+
