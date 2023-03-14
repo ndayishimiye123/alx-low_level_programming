1cc482fda7b7f17c7bce96282b764151f9d4fb16
ndayishimiye <ndayishimiye123@gmail.com>
2023-03-14 12:47:55 -0700

    Update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
  * count_word - helper function to count the number of words in a string
  * @s: string to evaluate
@@ -9,22 +8,20 @@
  */
 int count_word(char *s)
 {
-        int flag, c, w;
-
-        flag = 0;
-        w = 0;
-        for (c = 0; s[c] != '\0'; c++)
-        {
-                if (s[c] == ' ')
-                        flag = 0;
+int flag, c, w;
+flag = 0;
+w = 0;
+for (c = 0; s[c] != '\0'; c++)
+{
+if (s[c] == ' ')
+flag = 0;
 else if (flag == 0)
-                {
+{
 flag = 1;
-                        w++;
-                }
-        }
-
-        return (w);
+w++;
+}
+}
+return (w);
 }
 
 /**
